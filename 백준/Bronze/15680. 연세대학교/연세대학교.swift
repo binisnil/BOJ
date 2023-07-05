726fc224904f8a11f440e86a4e444cda0cd6c460
binisnil <109334968+binisnil@users.noreply.github.com>
2023-07-05 17:17:39 +0900

    [Bronze V] Title: 연세대학교, Time: 12 ms, Memory: 79508 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+var input = Int(readLine()!)!
+if input == 0 {
+    print("YONSEI")
+} else if input == 1 {
+    print("Leading the Way to the Future")
+} else {
+    print("")
+}
